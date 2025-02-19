5214a306de39265f74a8b6c3d1af29c29f22f488
James Michael C. Andal <andaljamesmichael_bsit@plmun.edu.ph>
2025-02-19 15:43:18 +0800

    tblStudents

--- SQL_Lab_Project/Scripts/02_insert_data.sql
+++ SQL_Lab_Project/Scripts/02_insert_data.sql
@@ -1,15 +1,68 @@
-/*
-    Filename:       02_insert_data.sql
-    Purpose:        Insert data into the database
-    Instructions:
-    1. Insert the following data into the tblStudents table:
-        - student_name: John Doe
-        - student_email: johndoe@gmail.com
-        - student_phone: 123-456-7890
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2025 at 08:38 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-        - student_name: Jane Doe
-        - student_email: janedoe@gmail.com
-        - student_phone: 123-456-7890
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-    2. Place your insert queries below this comment
-*/
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tblstudents`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudents`
+--
+
+CREATE TABLE `tblstudents` (
+  `student_id` int(11) NOT NULL,
+  `student_name` varchar(50) DEFAULT NULL,
+  `student_email` varchar(50) DEFAULT NULL,
+  `student_phone` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblstudents`
+--
+
+INSERT INTO `tblstudents` (`student_id`, `student_name`, `student_email`, `student_phone`) VALUES
+(1, 'John Doe', 'johndoe@gmail.com', '123-456-7890'),
+(2, 'Jane Doe', 'janedoe@gmail.com', '123-456-7890');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
