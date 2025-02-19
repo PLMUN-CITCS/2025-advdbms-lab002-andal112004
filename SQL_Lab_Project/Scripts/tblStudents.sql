a67f8d9d36bbdaf0c5b286331164f23c76521fc2
James Michael C. Andal <andaljamesmichael_bsit@plmun.edu.ph>
2025-02-19 15:23:32 +0800

    Completed assignment: Created tables and inserted data

---
+++ SQL_Lab_Project/Scripts/tblStudents.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2025 at 08:19 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tblstudents`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblStudents`
+--
+
+CREATE TABLE `tblstudents` (
+  `student_id` int(11) NOT NULL,
+  `student_name` varchar(50) DEFAULT NULL,
+  `student_email` varchar(50) DEFAULT NULL,
+  `student_phone` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblStudents`
+--
+
+INSERT INTO `tblStudents` (`student_id`, `student_name`, `student_email`, `student_phone`) VALUES
+(1, 'John Doe', 'johndoe@gmail.com', '123-456-7890'),
+(2, 'Jane Doe', 'janedoe@gmail.com', '123-456-7890');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblStudents`
+--
+ALTER TABLE `tblStudents`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblStudents`
+--
+ALTER TABLE `tblStudents`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
